520ffe7f7027986733cb9e640d04b2a9c36d128b
Michelle Findlay <mf@lp0.uk>
2026-08-16 21:02:45 +0100

    backend urls set.

--- Web/index.php
+++ Web/index.php
@@ -70,7 +70,7 @@ declare(strict_types=1);
 const WEB_URL         = 'https://dumpsterfire.uk';
 const API_URL         = 'https://api.dumpsterfire.uk';
 const STAGING_WEB_URL = 'https://dev.dumpsterfire.uk';
-const STAGING_API_URL = 'https://dev.api.dumpsterfire.uk';
+const STAGING_API_URL = 'https://dev.dumpsterfire.uk/api';
 
 /* ------------------------------------------------------------------ *
  * The scale. Masses are order-of-magnitude estimates and are not
